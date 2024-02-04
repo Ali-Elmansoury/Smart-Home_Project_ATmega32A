a22da4174d8f3632c14d656df4b10427eaf66c4d
MrDark0o0 <85578975+MrDark0o0@users.noreply.github.com>
2024-02-04 14:43:54 +0200

    MM74C922 Keypad Encoder Driver

---
+++ Amit Graduation Project/GccApplication1/GccApplication1/hal/MM74C922/Inc/MM74C922.h
@@ -0,0 +1,9 @@
+#ifndef MM74C922_H_
+#define MM74C922_H_
+
+#include "std_types.h"
+
+void MM74C922_Init(u8 portId, u8 pinId_S , u8 pinId_E);
+void MM74C922_Decode(u8 *data);
+
+#endif /* MM74C922_H_ */
---
+++ Amit Graduation Project/GccApplication1/GccApplication1/hal/MM74C922/Src/MM74C922.c
@@ -0,0 +1,36 @@
+#include MM74C922.h
+
+static u8 PortId;
+static u8 PinId_S;
+
+static u8 Table[16] = {0x00, 0x01, 0x02, 0x03,
+                       0x04, 0x05, 0x06, 0x07,
+                       0x08, 0x09, 0x0A, 0x0B,
+                       0x0C, 0x0D, 0x0E, 0x0F};
+
+void MM74C922_Init(u8 portId, u8 pinId_S , u8 pinId_E)
+{
+    #if (pinId_S < pinId_E)
+        #if ((pinId_E-pinId_S)!= 3)
+            #error "Invalid pin configuration"
+        #endif
+        for (u8 i = pinId_S; i <= pinId_E; i++)
+        {
+            dio_setPinDirection(portId, i, INPUT);
+        }
+        PortId = portId;
+        PinId_S = pinId_S;
+    #else
+        #error "Invalid pin configuration"
+    #endif
+}
+
+void MM74C922_Decode(u8 *data)
+{
+    u8 temp = 0;
+    for (u8 i = PinId_S; i < 4; i++)
+    {
+        temp |= (dio_readChannel(PortId, PinId_S + i) << i);
+    }
+    *data = Table[temp];
+}
