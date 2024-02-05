449ed4ed8d27ec7d31db6a8c666d46cd18aac9b7
Ali Elmansoury <73143531+Ali-Elmansoury@users.noreply.github.com>
2024-02-05 06:04:09 +0200

    Delete Amit Graduation Project/GccApplication1 directory

--- Amit Graduation Project/GccApplication1/GccApplication1/library/bit_math.h
+++
@@ -1,9 +0,0 @@
-#ifndef _bit_math_h_
-#define _bit_math_h
-
-#define SET_BIT(reg, bit_num) (reg|=(1<<bit_num))
-#define CLEAR_BIT(reg, bit_num) (reg&=~(1<<bit_num))
-#define CHECK_BIT(reg, bit_num) ((reg>>bit_num)&1)
-#define FLIP_BIT(reg, bit_num) (reg^=(1<<bit_num))
-
-#endif
